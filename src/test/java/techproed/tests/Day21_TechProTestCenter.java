65909a0a8474b6299302e8ffef61ef874b35cb84
Fatma Ozmen <fatmaozmen.be@gmail.com>
2023-01-27 22:02:42 +0100

    27.01

--- src/test/java/techproed/tests/Day21_TechProTestCenter.java
+++ src/test/java/techproed/tests/Day21_TechProTestCenter.java
@@ -1,6 +1,8 @@
 package techproed.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import techproed.pages.TechproHomePage;
 import techproed.pages.TechproLoginPage;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
@@ -9,10 +11,26 @@ public class Day21_TechProTestCenter {
 
     @Test
     public void testName() {
-        Driver.getDriver().get("https://testcenter.techproeducation.com/index.php?page=form-authentication");
-        TechproLoginPage techproLoginPage=new TechproLoginPage();
-        techproLoginPage.username.sendKeys("techproed");
+        Driver.getDriver().get(ConfigReader.getProperty("techpro_test_url"));
+        TechproLoginPage techproLoginPage = new TechproLoginPage();
+        TechproHomePage techproHomePage = new TechproHomePage();
+
+        techproLoginPage.username.sendKeys(ConfigReader.getProperty("techpro_test_username"));
         techproLoginPage.password.sendKeys(ConfigReader.getProperty("techpro_test_password"));
         techproLoginPage.submit.click();
+
+//      ASSERTION
+//        Login yapildi. Driver su an Home Pagede
+        Assert.assertTrue(techproHomePage.homeHeader.isDisplayed());
+
+//        Sayfadan cikis yap ve cikis yapildigini test et
+        techproHomePage.logoutButton.click();
+
+        // cikis olduguna dair assertion yap
+        Assert.assertTrue(techproLoginPage.submit.isDisplayed());
+
+        //Driver i kapat
+        Driver.closeDriver();
+
     }
 }
